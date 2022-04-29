6888524b0f7dac0d86c8f681cb65c0275b303ba1
Rohit Singh Katiyar <rohitsk@tech-radix.com>
2022-04-29 12:41:33 +0530

    Made example of states

--- App.js
+++ App.js
@@ -1,28 +1,39 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-
-import React from 'react';
+import React , {useState} from 'react';
 
 import {
   StyleSheet,
-  useColorScheme,
   View,
   Text,
   Button,
-  Linking
 } from 'react-native';
-
+// Changing the text dynamically
 const App = () => {
+  //Made a function that will toggle the name in state
+  const onClickHandler=() =>{
+    if(name==='Rohit')
+    {
+      setName('Mohit')
+      setStatus(true)
+    }
+    else 
+    {
+      setName('Rohit')
+      setStatus(false)
+    }
+  }
+  // Make a hook and give it a relavent name and default value if required
+  const [name, setName]= useState("Rohit")
+  // We can also define states having objects
+  const [lesson, setLesson]= useState({ number : 2 , about : "States" })
+  // State using boolean value 
+  const [status, setStatus] = useState(false)
   return (
    <View style={styles.body}>
-     <Text style={styles.text}>Hello world</Text>
-     <Button style={styles.btn_col} onPress={()=>{Linking.openURL('https://www.google.com')}} title='Click me'></Button>
+     <Text style={styles.text}>My name is: {name}</Text>
+     <Text style={styles.text}>This is {lesson.number} topic and it's ablout {lesson.about}</Text>
+     {/* Used the boolean value to change styling of the text */}
+     <Text style={status? styles.true: styles.false}>"Status" state have boolean value of "{JSON.stringify(status)}" right now</Text>
+     <Button  color={'#fec8d8'}  onPress={onClickHandler} title='Update state'></Button>
    </View>
   );
 };
@@ -37,12 +48,20 @@ const styles = StyleSheet.create({
   text:{
     color:'black',
     fontSize: 25,
-
+    fontStyle:'italic',
+    fontWeight:'600',
   },
-  btn_col:{
-    backgroundColor: '#ffff',
-
+  true:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    color:'#008000'
+  },
+  false:{
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#820000'
   }
+
 });
 
 export default App;
