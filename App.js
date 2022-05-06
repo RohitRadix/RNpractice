1b86aeeb858e51199944a05b9fae10ca6fc7572e
Rohit Singh Katiyar <rohitsk@tech-radix.com>
2022-05-06 10:37:42 +0530

    Made excescise

--- App.js
+++ App.js
@@ -4,20 +4,39 @@ import {
   StyleSheet,
   View,
   Text,
-  Button,
 } from 'react-native';
 // Undestanding Responsive UI and flexbox
 const App = () => {
   return (
     <View style={styles.body}>
-      <View style={styles.view1}>
-        <Text style={styles.text}>1</Text>
+      <View style={styles.row}>
+        <View style={styles.view1}>
+          <Text style={styles.text}>1</Text>
+        </View>
+        <View style={styles.view2}>
+          <Text style={styles.text}>2</Text>
+        </View>
+        <View style={styles.view3}>
+          <Text style={styles.text}>3</Text>
+        </View>
       </View>
-      <View style={styles.view2}>
-        <Text style={styles.text}>2</Text>
+      <View style={styles.row}>
+        <View style={styles.view4}>
+          <Text style={styles.text}>4</Text>
+        </View>
       </View>
-      <View style={styles.view3}>
-        <Text style={styles.text}>3</Text>
+      <View style={styles.row}>
+        <View style={styles.view5}>
+          <Text style={styles.text}>5</Text>
+        </View>
+      </View>
+      <View style={styles.big_row}>
+        <View style={styles.view6}>
+          <Text style={styles.text}>6</Text>
+      </View>
+          <View style={styles.view7}>
+            <Text style={styles.text}>7</Text>
+        </View>
       </View>
     </View>
   );
@@ -25,53 +44,73 @@ const App = () => {
 
 const styles = StyleSheet.create({
   body: {
-    // Flex 1 spreads the view to whole area
     flex: 1,
-    // Default flex direction is col
-    // flexDirection:"column",
-    // flex direction is col reverse , it reverses the order of items
-    // flexDirection:"column-reverse",
-    // flex direction set to row 
-    // flexDirection:"row",
-    // flex direction set to row reverse
-    // flexDirection:"row-reverse",
+    flexDirection: 'column',
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  big_row:{
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#ffffff',
     alignItems: 'stretch',
     justifyContent: 'center',
-    // borderWidth: 5,
-    // borderColor: 'grey',
-
-
   },
   text: {
     fontSize: 25,
     fontWeight: 'bold',
-    color: 'black'
+    color: 'black',
+    margin: 10,
   },
   view1: {
-
-    // width: 100,
-    // height: 100,
-    flex:1,
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#fad6a5',
   },
   view2: {
     flex: 2,
-    // width: 100,
-    // height: 100,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#faa6a5',
   },
   view3: {
-    flex:1,
-    // width: 100,
-    // height: 100,
+    flex: 3,
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#faf6a5',
-  }
+  },
+  view4: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'red',
+  },
+  view5: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'green',
+  },
+  view6: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  view7: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'blue',
+  },
+
 });
 
 export default App;
