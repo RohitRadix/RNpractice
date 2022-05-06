1e85e6008e4764082684d010ae9a079605d78e26
Rohit Singh Katiyar <rohitsk@tech-radix.com>
2022-05-06 12:52:48 +0530

    Made List ScrollView and RefreshControl

--- App.js
+++ App.js
@@ -4,41 +4,44 @@ import {
   StyleSheet,
   View,
   Text,
+  ScrollView,
+  RefreshControl
 } from 'react-native';
-// Undestanding Responsive UI and flexbox
+//Using List ScrollView and Seeing how to ctrl refresh
 const App = () => {
+  let [items, setItems] = useState([
+    { key: 1, value: 'Item 1' },
+    { key: 2, value: 'Item 2' },
+    { key: 3, value: 'Item 3' },
+    { key: 4, value: 'Item 4' },
+    { key: 5, value: 'Item 5' },
+    { key: 6, value: 'Item 6' },
+    { key: 7, value: 'Item 7' },
+    { key: 8, value: 'Item 8' },
+    { key: 9, value: 'Item 9' },
+    { key: 10, value: 'Item 10' },
+    { key: 11, value: 'Item 11' },
+    { key: 12, value: 'Item 12' },
+  ]);
+  const [refresh, setRefresh] = useState(false)
+  const onRefresh = () => {
+    setRefresh(true)
+    setItems([...items, { key: 33, value: 'Item 33' }])
+    setRefresh(false)
+  }
   return (
-    <View style={styles.body}>
-      <View style={styles.row}>
-        <View style={styles.view1}>
-          <Text style={styles.text}>1</Text>
-        </View>
-        <View style={styles.view2}>
-          <Text style={styles.text}>2</Text>
-        </View>
-        <View style={styles.view3}>
-          <Text style={styles.text}>3</Text>
-        </View>
-      </View>
-      <View style={styles.row}>
-        <View style={styles.view4}>
-          <Text style={styles.text}>4</Text>
-        </View>
-      </View>
-      <View style={styles.row}>
-        <View style={styles.view5}>
-          <Text style={styles.text}>5</Text>
-        </View>
-      </View>
-      <View style={styles.big_row}>
-        <View style={styles.view6}>
-          <Text style={styles.text}>6</Text>
-      </View>
-          <View style={styles.view7}>
-            <Text style={styles.text}>7</Text>
-        </View>
-      </View>
-    </View>
+    // On refresh it will add one more item in the objects
+    <ScrollView refreshControl={<RefreshControl refreshing={refresh} onRefresh={onRefresh} colors={['#fcf8']} />} style={styles.body} refre >
+      {
+        items.map((obj) => {
+          return (
+            <View style={styles.items} key={obj.key}>
+              <Text style={styles.text}>{obj.value}</Text>
+            </View>
+          )
+        })
+      }
+    </ScrollView >
   );
 };
 
@@ -47,69 +50,21 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
+
   },
-  row: {
-    flexDirection: "row",
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  big_row:{
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#ffffff',
-    alignItems: 'stretch',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 25,
-    fontWeight: 'bold',
-    color: 'black',
-    margin: 10,
-  },
-  view1: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fad6a5',
-  },
-  view2: {
-    flex: 2,
+  items: {
     alignItems: 'center',
     justifyContent: 'center',
     backgroundColor: '#faa6a5',
+    margin: 10,
+
   },
-  view3: {
-    flex: 3,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#faf6a5',
+  text: {
+    fontSize: 35,
+    margin: 10
+
   },
-  view4: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'red',
-  },
-  view5: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'green',
-  },
-  view6: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'white',
-  },
-  view7: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'blue',
-  },
+
 
 });
 
