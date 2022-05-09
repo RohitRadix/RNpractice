aadc2790287d59d301e2741e82961abe1a9d8329
Rohit Singh Katiyar <rohitsk@tech-radix.com>
2022-05-09 16:31:25 +0530

    made flatlist

--- App.js
+++ App.js
@@ -5,51 +5,91 @@ import {
   View,
   Text,
   ScrollView,
-  RefreshControl
+  RefreshControl, FlatList, SectionList
 } from 'react-native';
 //Using List ScrollView and Seeing how to ctrl refresh
 const App = () => {
-  let [items, setItems] = useState([
-    { key: 1, value: 'Item 1' },
-    { key: 2, value: 'Item 2' },
-    { key: 3, value: 'Item 3' },
-    { key: 4, value: 'Item 4' },
-    { key: 5, value: 'Item 5' },
-    { key: 6, value: 'Item 6' },
-    { key: 7, value: 'Item 7' },
-    { key: 8, value: 'Item 8' },
-    { key: 9, value: 'Item 9' },
-    { key: 10, value: 'Item 10' },
-    { key: 11, value: 'Item 11' },
-    { key: 12, value: 'Item 12' },
+  let [data, setData] = useState([
+    { value: 'Item 1' },
+    { value: 'Item 2' },
+    { value: 'Item 3' },
+    { value: 'Item 4' },
+    { value: 'Item 5' },
+    { value: 'Item 6' },
+    { value: 'Item 7' },
+    { value: 'Item 8' },
+    { value: 'Item 9' },
+    { value: 'Item 10' },
+    { value: 'Item 11' },
+    { value: 'Item 12' },
   ]);
+  const DATA = [
+    {
+      title: 'Title 1',
+      data: ['Item 1-1', 'Item 2-2', 'Item 3-3'],
+    },
+    {
+      title: 'Title 2',
+      data: ['Item 2-1', 'Item 2-2'],
+    },
+    {
+      title: 'Title 3',
+      data: ['Item 3-1',],
+    },
+  ]
+
   const [refresh, setRefresh] = useState(false)
   const onRefresh = () => {
     setRefresh(true)
-    setItems([...items, { key: 33, value: 'Item 33' }])
+    setData([...data, { value: 'Item 33' }])
     setRefresh(false)
   }
   return (
-    // On refresh it will add one more item in the objects
-    <ScrollView refreshControl={<RefreshControl refreshing={refresh} onRefresh={onRefresh} colors={['#fcf8']} />} style={styles.body} refre >
-      {
-        items.map((obj) => {
-          return (
-            <View style={styles.items} key={obj.key}>
-              <Text style={styles.text}>{obj.value}</Text>
-            </View>
-          )
-        })
-      }
-    </ScrollView >
-  );
+    //In order to show nested array
+    <SectionList
+      keyExtractor={(item, index) => index.toString()}
+      sections={DATA}
+      renderItem={({ item }) => {
+        return (
+          <Text style={styles.text}>{item}</Text>
+        )
+      }}
+      renderSectionHeader={({ section }) => {
+        return (
+          <View style={styles.items}>
+            <Text style={styles.text}>{section.title}</Text>
+          </View>
+        )
+      }}
+    />
+
+    //Use flat list in place pf scroll view because of more props
+    //   <FlatList
+    //     // numColumns={2}
+    //     keyExtractor={(item, index) => index.toString()}
+    //     data={data}
+    //     renderItem={({ item }) => {
+    //       return (
+    //         <View style={styles.items}>
+    //           <Text style={styles.text}>{item.value}</Text>
+    //         </View>
+    //       )
+    //     }}
+    //     refreshControl={
+    //       <RefreshControl
+    //         refreshing={refresh}
+    //         onRefresh={onRefresh}
+    //         colors={['#fcf8']} />}
+    //   />
+    // );
+  )
 };
 
 const styles = StyleSheet.create({
   body: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: '#ffffff',
+    backgroundColor: '#fff',
 
   },
   items: {
@@ -57,11 +97,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     backgroundColor: '#faa6a5',
     margin: 10,
-
   },
   text: {
     fontSize: 35,
-    margin: 10
+    margin: 10,
+    color: 'black'
 
   },
 
