d0192b0434ff5ff5d082670b86ba212fa493c7b4
Rohit Singh Katiyar <rohitsk@tech-radix.com>
2022-05-10 14:46:00 +0530

    Made different types of buttons

--- App.js
+++ App.js
@@ -5,13 +5,23 @@ import {
   View,
   Text,
   ScrollView,
-  RefreshControl, TextInput
+  RefreshControl,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  TouchableHighlight
 } from 'react-native';
 //Using List ScrollView and Seeing how to ctrl refresh
 const App = () => {
 
   const [name, setName] = useState('')
   const [age, setAge] = useState()
+  let [submitted, setSubmitted] = useState(false)
+  //Logic to toggle submitted props
+  const submit = () => {
+    setSubmitted(!submitted)
+
+  }
   return (
     <View style={styles.body}>
       <Text style={styles.text}>Enter your name</Text>
@@ -30,15 +40,39 @@ const App = () => {
           setAge(val)
         }}
       />
-      {/* This statement will only appear when age is not null */}
+      {/* Button title will change acc. to the state  */}
+      <Button title={submitted ? 'Clear' : 'Submit'}
+        onPress={submit}
+        // The button will be disabled after it is clicked
+        // disabled={submitted}
+        color='#000'
+      />
+
+      {/* Since Button has very limited props we eill use othe alt  */}
+      <TouchableOpacity
+        style={styles.button}
+        onPress={submit}
+      >
+        <Text style={styles.subText}>{submitted ? 'Touchable Clear' : 'Touchable Submit'}</Text>
+      </TouchableOpacity>
+
+      {/* We will use touchable heighlight as we can give a color when it is clicked */}
+      <TouchableHighlight
+        style={styles.button}
+        onPress={submit}
+        //This color is shown when button is clicked
+        underlayColor='#fcf'
+      >
+        <Text style={styles.subText}>{submitted ? 'TouchableHighlight Clear' : 'TouchableHighlight Submit'}</Text>
+      </TouchableHighlight>
+
+
+      {/* This statement will only appear when button is pressed */}
       {
-        age != null &&
+        submitted === true &&
         <Text style={styles.subText}>Your name is {name} and you are {age} years old </Text>
       }
-
     </View>
-
-
   );
 };
 
@@ -69,8 +103,16 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     width: 260,
     fontSize: 20,
-    textAlign: 'center'
-  }
+    textAlign: 'center',
+    marginBottom: 26,
+
+  },
+  button: {
+    backgroundColor: 'grey',
+    borderColor: 'black',
+    borderRadius: 5,
+    marginTop: 20,
+  },
 
 
 });
